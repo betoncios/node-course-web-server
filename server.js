cc4f2b491410eb0b85b39c04032e4efeb07b7448
Humberto <betoncios@gmail.com>
2017-11-05 14:27:27 -0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('welcomeMessage', (message) => {
+	return message.toUpperCase();
+});
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	console.log(`${new Date().toString()}: ${req.method} ${req.url}`);
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcome: 'Welcome'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
